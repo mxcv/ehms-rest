8dfa238153681645d9c20cf15f721729b923e838
Max <mxcs02@gmail.com>
2024-03-15 02:17:58 +0200

    Implement part of employee router

--- src/routers/employee-router.ts
+++ src/routers/employee-router.ts
@@ -5,13 +5,20 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const employees = employeeRepository.readAll();
-    holidayRequestRepository.joinApprovedWithEmployees(employees);
-    res.render('employees.ejs', { employees })
-})
+    if (req.query.joinHolidays)
+        holidayRequestRepository.joinApprovedWithEmployees(employees);
+    return res.json(employees);
+});
 
-router.post('/', (req, res) => {
-    employeeRepository.create({ name: req.body.name });
-    res.redirect('/employees');
-})
+// router.get('/', (req, res) => {
+//     const employees = employeeRepository.readAll();
+//     holidayRequestRepository.joinApprovedWithEmployees(employees);
+//     res.render('employees.ejs', { employees })
+// })
+
+// router.post('/', (req, res) => {
+//     employeeRepository.create({ name: req.body.name });
+//     res.redirect('/employees');
+// })
 
 export default router;
